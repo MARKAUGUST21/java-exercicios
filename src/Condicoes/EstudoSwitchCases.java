b168f8f56df10ae53db0514cf73b979b2a313042
Marcos Pedro Camargo <mcmpgui12@gmail.com>
2025-04-19 01:54:39 -0300

    Estudos em java

---
+++ src/Condicoes/EstudoSwitchCases.java
@@ -0,0 +1,52 @@
+package Condicoes;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class EstudoSwitchCases {
+    public static void main(String[] args) {
+        /*
+        *SwitchCases: serve para gerar casos especificos
+        * Objetivo: Pedir ao usario escolher entre os Ninjas
+        **/
+
+        // Pedir para o usario
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        // Mostrar as opcoes para o usuario:
+
+        System.out.println(" Escolha o personagem: ");
+        System.out.println(" 1 - Naruto");
+        System.out.println(" 2 - Sasuke");
+        System.out.println(" 3 - Sakura");
+
+        // Pedir para o usaurio escolher uma opcao
+        int escolhaUs= scanner.nextInt();
+
+        System.out.println("Voce digitou o numero: " + escolhaUs);
+
+        // Reacao ao escolher um personagem
+
+        switch (escolhaUs){
+            case 1:
+                System.out.println("O usuario escolheu o Naruto ");
+                break;
+            case 2:
+                System.out.println("O usuario escolheu o Sasuke");
+                break;
+            case 3:
+                System.out.println("O usuario escolheu  a 1Sakura");
+            default:
+                System.out.println("Voce nao digitou uma resposta valida! ");
+
+
+        }
+
+
+
+        //Fechar minha caixa
+        scanner.close();
+    }
+}
---
+++ src/Condicoes/IfEElse.java
@@ -0,0 +1,39 @@
+package Condicoes;
+
+public class IfEElse {
+
+    public static void main(String[] args) {
+
+        /*
+        * If e Else - Condiecoes
+        * Objetivo da aula, passar o ninja de nivel de acordod de missoes
+        */
+
+        String rank ="";
+        String nome = "Naruto";
+        int idade = 20;
+        boolean hokage = false;
+        short numeroDemissoes = 20;
+
+        /*
+        * if (condicao) {resultado}
+        * else if (conficao) {resultado}
+        * else {resultado caso nada seja verdadeiro}
+        * */
+
+
+        // se (condicao) {faca isso}
+        if (numeroDemissoes == 10 && idade > 15){
+            System.out.println("Rank: Chunnin");
+        } else if (numeroDemissoes >= 20){
+            System.out.println("Rank: Jounin");
+        }else {
+            System.out.println("Rank: Gennim");
+        }
+
+
+    }
+
+
+
+}
